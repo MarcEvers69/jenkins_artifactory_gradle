97ae535b68b49e0a1e784a37eb2e610b1544b5b9
Marc Evers <marcevers@Marcs-MacBook-Pro.local>
2019-09-17 09:51:01 +0200

    test change to trigger release build

--- src/main/java/jenkins_artifactory_gradle/App.java
+++ src/main/java/jenkins_artifactory_gradle/App.java
@@ -5,7 +5,7 @@ package jenkins_artifactory_gradle;
 
 public class App {
     public String getGreeting() {
-        return "Hello world.";
+        return "Hello worlds.";
     }
 
     public static void main(String[] args) {
